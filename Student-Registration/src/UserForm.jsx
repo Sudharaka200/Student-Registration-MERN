a866d2bb2065d724f9b9ef4695ea4ebf87e63813
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2024-12-30 15:35:22 +0530

    user form

---
+++ Student-Registration/src/UserForm.jsx
@@ -0,0 +1,37 @@
+import { Button, Grid2, Input, Typography } from "@mui/material";
+
+const UserForm = (props) =>{
+    return(
+        <Grid2 
+            container
+            spacing={2}
+            sx={{
+                backgroundColor : "#ffff",  
+                marginBottom : '30px',
+                display : 'block'
+            }}
+        >
+            <Grid2 item xs={12}>
+                <Typography component={'h1'} sx={{color : '#00000000'}}> User Form</Typography>
+            </Grid2>
+            <Grid2 item xs={12} sm={6} sx={{display : 'flex'}}>
+                <Typography component={'label'} htmlFor="id" sx={{color : '#00000',marginRight : '20px',fontSize : '16px',width : '100px', display : 'block'}}>
+                    ID
+                </Typography>
+                <Input  type="number" id="id" name="id" sx={{width : '400px'}} value={''} onChange={e =>{}}/>
+            </Grid2>
+            <Grid2 item xs={12} sm={6} sx={{display : 'flex'}}>
+                <Typography component={'label'} htmlFor="id" sx={{color : '#00000',marginRight : '20px',fontSize : '16px',width : '100px', display : 'block'}}>
+                    Name
+                </Typography>
+                <Input  type="text" id="id" name="id" sx={{width : '400px'}} value={''} onChange={e =>{}}/>
+            </Grid2>
+            <Button sx={{margin: 'auto' ,marginTop : ' 20px', backgroundColor : '#00c6e6',color: '#000000', marginLeft:'15px', marginBottom: '20px', '&:hover':{opacity:'0.7',backgroundColor:'00c6e6'}}}>
+                Add
+            </Button>
+        </Grid2>
+    );
+
+}
+
+export default UserForm;
