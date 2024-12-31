49a1058545b13b35fb8dc40f65a47429218946cf
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2024-12-31 20:06:29 +0530

    node

---
+++ Student-Registration-Backend/server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const port = 3001;
+const host = '127.0.0.1';
+
+
+const server = app.listen(3001,'127.0.0.1', () => {
+    console.log(`Node server is Lis ${server.address().port}`)
+});
