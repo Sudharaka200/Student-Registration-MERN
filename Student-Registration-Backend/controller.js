05227ce0a812278285dc2187972e96252fcd5a7f
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-01-02 01:14:24 +0530

    backend

--- Student-Registration-Backend/controller.js
+++ Student-Registration-Backend/controller.js
@@ -1,22 +1,52 @@
-const users = [
-    {
-        id: 1,
-        name: 'Prasad',
-    },
-    {
-        id: 2,
-        name: 'Prasadi',
-    },
-];
+const User = require('./model');
 
-const getUsers = (cb) => {
-    cb(users);
+const getUsers = (req, res, next) => {
+    User.find()
+        .then(response => {
+            res.json({response})
+        })
+        .catch(error => {
+            res.json({error: error})
+        });
 };
 
-const getUsersById = (id,cb) => {
-    const user = users.find(user => user.id == id);
-    cb(user);
-};
+const addUser = (req, res, next) => {
+    const user = new User({
+        id: req.body.id,
+        name: req.body.name,
+    });
+    user.save()
+        .then(response => {
+            res.json({response})
+        })
+        .catch(error => {
+            res.json({error: error})
+        });
+}
+
+const updateUser = (req, res, next) => {
+    const{ id, name } = req.body;
+    User.updateOne({ id: id }, { $set: {name: name } })
+        .then(response => {
+            res.json({response})
+        })
+        .catch(error => {
+            res.json({error: error})
+        });
+}
+
+const deletUser = (req, res, next) => {
+    const id = req.body.id;
+    User.deleteOne({ id: id})
+        .then(response => {
+            res.json({response})
+        })
+        .catch(error => {
+            res.json({error: error})
+        });
+}
 
 exports.getUsers = getUsers;
-exports.getUsersById = getUsersById;
+exports.addUser = addUser;
+exports.updateUser = updateUser;
+exports.deletUser = deletUser;
--- Student-Registration-Backend/server.js
+++ Student-Registration-Backend/server.js
@@ -1,8 +1,26 @@
 const app = require("./app");
 const port = 3001;
 const host = '127.0.0.1';
+const mongoose = require('mongoose');
+const router = require('./router');
 
+app.use(cors());
+app.use(express.json());
+
+const url = 'mongodb+srv://user_data:<Sudharaka@24173>@cluster0.l3n9q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const connect = async () => {
+    try{
+        await mongoose.connect(url);
+        console.log('Connected to MongoDB');
+    }
+    catch(error){
+        console.log('MongoDB Error:' , error);
+    }
+};
 
 const server = app.listen(3001,'127.0.0.1', () => {
     console.log(`Node server is Lis ${server.address().port}`)
 });
+
+app.use('/api', router);
